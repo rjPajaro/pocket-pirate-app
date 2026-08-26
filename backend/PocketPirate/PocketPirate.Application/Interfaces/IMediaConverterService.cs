b366080baeefa90e344ad224bf05eee73723238a
Randall Joseph Pajaro <73212561+rjPajaro@users.noreply.github.com>
2026-08-27 03:06:04 +0800

    Added media conversion from urls to mp3/mp4 (#1)
    * Fix duplicate header in README.md
    
    * Added endpoints for downloading media from urls to either mp3 or mp4
    
    * Remove ffmpeg.exe from tracking, update .gitignore

---
+++ backend/PocketPirate/PocketPirate.Application/Interfaces/IMediaConverterService.cs
@@ -0,0 +1,8 @@
+﻿namespace PocketPirate.Application.Interfaces
+{
+    public interface IMediaConverterService
+    {
+        Task<string> ConvertToMp3(string url);
+        Task<string> ConvertToMp4(string url);
+    }
+}
---
+++ backend/PocketPirate/PocketPirate/Controllers/MediaConverterController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PocketPirate.Application.Interfaces;
+
+namespace PocketPirate.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MediaConverterController : ControllerBase
+    {
+        private readonly IMediaConverterService _mediaConverterService;
+
+        public MediaConverterController(IMediaConverterService mediaConverterService)
+        {
+            _mediaConverterService = mediaConverterService;
+        }
+
+        [HttpPost("convert-to-mp3")]
+        public async Task<IActionResult> ConvertToMp3([FromBody] string url)
+        {
+            var filePath = await _mediaConverterService.ConvertToMp3(url);
+            var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.None, 4096, FileOptions.DeleteOnClose);
+            return File(stream, "audio/mpeg", Path.GetFileName(filePath));
+        }
+
+        [HttpPost("convert-to-mp4")]
+        public async Task<IActionResult> ConvertToMp4([FromBody] string url)
+        {
+            var filePath = await _mediaConverterService.ConvertToMp4(url);
+            var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.None, 4096, FileOptions.DeleteOnClose);
+            return File(stream, "video/mp4", Path.GetFileName(filePath));
+        }
+    }
+}
